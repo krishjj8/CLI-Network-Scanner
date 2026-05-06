4d127ab51064c0fddb18676c55f5532571006782
krishjj8 <krishkumarj81@gmail.com>
2026-05-06 19:21:52 +0530

    feat:Day 3 Sorted the output and added CLI Flags

--- main.go
+++ main.go
@@ -1,13 +1,15 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"net"
+	"sort"
 	"sync"
 	"time"
 )
 
-func scanport(host string, port int, wg *sync.WaitGroup) {
+func scanport(host string, port int, wg *sync.WaitGroup, results chan int) {
 	defer wg.Done()
 	target := fmt.Sprintf("%s:%d", host, port)
 	conn, err := net.DialTimeout("tcp", target, 3*time.Second)
@@ -15,16 +17,39 @@ func scanport(host string, port int, wg *sync.WaitGroup) {
 		return
 	}
 	defer conn.Close()
-	fmt.Printf("[+] Port %d is open\n", port)
+	results <- port
+
 }
 
 func main() {
-	host := "scanme.nmap.org"
+	//flags
+	host := flag.String("host", "scanme.nmap.org", "Host to scan")
+	startPort := flag.Int("start", 1, "Start of port range")
+	endPort := flag.Int("end", 1024, "end of port range")
+	flag.Parse()
+
 	var wg sync.WaitGroup
-	for port := 1; port <= 1024; port++ {
+	results := make(chan int)
+	var openPorts []int
+
+	go func() {
+		for p := range results {
+			openPorts = append(openPorts, p)
+		}
+	}()
+	fmt.Printf("Scanning %s from %d to %d...\n", *host, *startPort, *endPort)
+
+	for port := *startPort; port <= *endPort; port++ {
 		wg.Add(1)
-		go scanport(host, port, &wg)
+		go scanport(*host, port, &wg, results)
 	}
 	wg.Wait()
+	close(results)
+
+	sort.Ints(openPorts)
+	for _, port := range openPorts {
+		fmt.Printf("[+] Port %d is open\n", port)
+
+	}
 	fmt.Println("Scan complete")
 }
