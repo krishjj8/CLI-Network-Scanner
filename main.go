8c36e78f2958765e4e59c9bb16a3d0f8db0a575b
krishjj8 <krishkumarj81@gmail.com>
2026-05-07 13:20:03 +0530

    feat:Day 4 CIDR subnet scanning

--- main.go
+++ main.go
@@ -4,52 +4,62 @@ import (
 	"flag"
 	"fmt"
 	"net"
+	"net/netip"
 	"sort"
 	"sync"
 	"time"
 )
 
-func scanport(host string, port int, wg *sync.WaitGroup, results chan int) {
+func scanport(ip netip.Addr, port int, wg *sync.WaitGroup, results chan<- string) {
 	defer wg.Done()
-	target := fmt.Sprintf("%s:%d", host, port)
+	target := fmt.Sprintf("%s:%d", ip, port)
 	conn, err := net.DialTimeout("tcp", target, 3*time.Second)
 	if err != nil {
 		return
 	}
 	defer conn.Close()
-	results <- port
+	results <- fmt.Sprintf("%s:%d", ip, port)
 
 }
 
 func main() {
 	//flags
-	host := flag.String("host", "scanme.nmap.org", "Host to scan")
-	startPort := flag.Int("start", 1, "Start of port range")
-	endPort := flag.Int("end", 1024, "end of port range")
+	cidrinput := flag.String("cidr", "45.33.32.156/32", "Network Range to scan")
+	startPort := flag.Int("start", 80, "Start port")
+	endPort := flag.Int("end", 85, "end port")
 	flag.Parse()
 
+	prefix, err := netip.ParsePrefix(*cidrinput)
+	if err != nil {
+		fmt.Printf("Invalid CIDR:%v", err)
+		return
+
+	}
+
 	var wg sync.WaitGroup
-	results := make(chan int)
-	var openPorts []int
+	results := make(chan string)
+	var openPorts []string
 
 	go func() {
 		for p := range results {
 			openPorts = append(openPorts, p)
 		}
 	}()
-	fmt.Printf("Scanning %s from %d to %d...\n", *host, *startPort, *endPort)
+	fmt.Printf("Scanning %s from %d to %d...\n", *cidrinput, *startPort, *endPort)
 
-	for port := *startPort; port <= *endPort; port++ {
-		wg.Add(1)
-		go scanport(*host, port, &wg, results)
+	for addr := prefix.Addr(); prefix.Contains(addr); addr = addr.Next() {
+		for port := *startPort; port <= *endPort; port++ {
+			wg.Add(1)
+			go scanport(addr, port, &wg, results)
+		}
 	}
 	wg.Wait()
 	close(results)
 
-	sort.Ints(openPorts)
+	sort.Strings(openPorts)
 	for _, port := range openPorts {
-		fmt.Printf("[+] Port %d is open\n", port)
+		fmt.Printf("[+] Port %s is open\n", port)
 
 	}
-	fmt.Println("Scan complete")
+	fmt.Printf("\nScan complete. Found %d open ports.\n", len(openPorts))
 }
